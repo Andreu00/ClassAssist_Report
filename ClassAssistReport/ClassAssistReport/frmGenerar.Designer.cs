32f644ece3aaaa6e5c54785e25e45a59f1b43d09
Francisco Javier Andreu García <javiandreugarcia@gmail.com>
2024-06-12 13:55:57 +0200

    add MaterialDesign
    He añadido el materialdesign para que mantuviera el diseño de la aplicacion principal

--- ClassAssistReport/ClassAssistReport/frmGenerar.Designer.cs
+++ ClassAssistReport/ClassAssistReport/frmGenerar.Designer.cs
@@ -41,14 +41,14 @@
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
             this.crystalReportViewer1.ReportSource = this.CrystalReport11;
-            this.crystalReportViewer1.Size = new System.Drawing.Size(835, 488);
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1179, 564);
             this.crystalReportViewer1.TabIndex = 0;
             // 
             // frmGenerar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(835, 488);
+            this.ClientSize = new System.Drawing.Size(1179, 564);
             this.Controls.Add(this.crystalReportViewer1);
             this.Name = "frmGenerar";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
